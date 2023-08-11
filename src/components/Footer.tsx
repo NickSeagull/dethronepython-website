6fdcdab0cb08f55fe8a2fc3b35c387a364bf755a
juanjoman <j2rgez@gmail.com>
2023-08-11 15:57:48 +0100

    missing responsive - add_site

---
+++ src/components/Footer.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import LinkBox from './LinkBox';
+import ArrowRightIcon from '../icons/arrow_right_icon.svg';
+import PlusIcon from '../icons/plus_icon_reverse.svg';
+import { Link } from 'react-router-dom';
+
+const Footer: React.FC = () => {
+  return (
+    <div>
+      <div className='mt-40 md:max-w-4xl max-w-md mx-auto text-center '>
+        <h1 className='p-12 pb-5 text-3xl font-silkscreen'>Featured projects</h1>
+        <span className='w-1'>These projects have officially joined the cause:</span>
+        <div className='mt-10'>
+          <LinkBox to='' backgroundColor='black' textColor='white' icon={ArrowRightIcon}>
+            <p className='text-start font-bold'>eLLMental</p>
+            <p>An open-source library for Java</p>
+          </LinkBox>
+
+          <div className="relative -mt-2">
+            <LinkBox to='' backgroundColor='white' textColor='black' icon={PlusIcon}>
+              <p className='text-start'>
+                <span className='text-start font-bold'>Want to add your JVM AI project?</span> Find us on GitHub, send a PR with your project description, and we'll vote for it!
+              </p>
+            </LinkBox>
+          </div>
+        </div>
+      </div>
+      <div className="relative">
+        <img className='w-25' src='hell_background.png' alt='python-hell-2'/>
+        <span className='absolute top-1/4 left-1/2 transform -translate-x-1/2 -translate-y-1 hover:font-bold'>
+          Signed: <Link target='_blank' className='underline' to='https://theagilemonkeys.com'>The Agile Monkeys.</Link>
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/Intro.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import ArrowRightIcon from '../icons/arrow_right_icon.svg';
+import PlusIcon from '../icons/plus_icon.svg';
+
+const Intro: React.FC = () => {
+  return (
+    <div className='mt-6'>
+      <nav className="flex justify-evenly">
+        <Link to="/discord" target='blank' className="flex hover:font-bold">
+          <img src={ArrowRightIcon} alt="Discord" className="mr-2" />
+          <span>Join Discord</span>
+        </Link>
+        <Link to="/add-project" target='blank' className="flex hover:font-bold">
+          <img src={PlusIcon} alt="Project" className="mr-2" />
+          <span>Add My Project</span>
+        </Link>
+      </nav>
+      <div className="relative md:flex md:items-end justify-center mt-10">
+        <img src="hell_monsters_background.png" alt="Background" className="w-25" />
+        <div className="flex justify-center items-center absolute md:inset-auto md:bottom-10">
+          <span className="font-silkscreen text-center md:text-4xl text-2xl text-white md:max-w-2xl max-w-sm">   
+              We are not saying Python is s***... did we
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Intro;
